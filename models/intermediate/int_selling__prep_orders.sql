c470807133373e89bf66334299ec159eb5460482
Gustavo Girardi <gustavo.girardi@indicium.tech>
2025-03-27 20:17:04 +0000

    Limpando as colunas que não serão utilizadas

---
+++ models/intermediate/int_selling__prep_orders.sql
@@ -0,0 +1,44 @@
+with 
+    salesorderdetail as (
+        select *
+        from {{ ref ('stg_sales__salesorderdetail')}}
+    )
+
+    ,salesorderheader as (
+        select *
+        from {{ ref ('stg_sales__salesorderheader')}}
+    )
+
+    ,gross_total as (
+        select
+            salesorderdetail.pk_salesorderdetail 
+            , salesorderdetail.fk_salesorder 
+            , salesorderdetail.fk_product
+            , salesorderheader.fk_customer
+            , salesorderheader.fk_address
+            , salesorderheader.fk_creditcard
+            , salesorderheader.orderdate
+            , salesorderheader.duedate
+            , salesorderheader.shipdate
+            , salesorderheader.has_online_order
+            , case
+                when salesorderdetail.unitpricediscount > 0 then true
+                else false
+            end as has_discount
+            , salesorderheader.status
+            , salesorderdetail.orderqty
+            , salesorderdetail.unitprice
+            , salesorderdetail.orderqty * salesorderdetail.unitprice as gross_total
+            , cast(salesorderheader.freight / count(*) over(partition by fk_salesorder) as numeric(18,2)) as allocated_freight
+            , cast(salesorderheader.taxamt / count(*) over(partition by fk_salesorder) as numeric(18,2)) as allocated_taxamt            
+            , salesorderdetail.unitpricediscount
+        from
+            salesorderdetail
+        left join
+            salesorderheader on salesorderheader.pk_salesorder = salesorderdetail.fk_SALESORDER
+    )
+
+    select
+        *
+    from
+        gross_total
--- models/staging/sales/stg_sales__salesorderdetail.sql
+++ models/staging/sales/stg_sales__salesorderdetail.sql
@@ -6,16 +6,16 @@ with
 
     ,renomeado as (
         select 
-            cast(source_salesorderdetail.SALESORDERID as int) as fk_SALESORDER
-            ,cast(source_salesorderdetail.SALESORDERDETAILID as int) as pk_SALESORDERDETAILID
-            ,cast(source_salesorderdetail.CARRIERTRACKINGNUMBER as string) as CARRIERTRACKINGNUMBER
-            ,cast(source_salesorderdetail.ORDERQTY as int) as ORDERQTY
-            ,cast(source_salesorderdetail.PRODUCTID as int) as fk_PRODUCT
-            ,cast(source_salesorderdetail.SPECIALOFFERID as int) as fk_SPECIALOFFERID
-            ,cast(source_salesorderdetail.UNITPRICE as numeric(18,2)) as UNITPRICE
-            ,cast(source_salesorderdetail.UNITPRICEDISCOUNT as numeric(18,2)) as UNITPRICEDISCOUNT
-            ,cast(source_salesorderdetail.ROWGUID as string) as ROWGUID_source_salesorderdetail
-            ,cast(source_salesorderdetail.MODIFIEDDATE as date) as MODIFIEDDATE_source_salesorderdetail
+            cast(source_salesorderdetail.salesorderid as int) as fk_salesorder
+            , cast(source_salesorderdetail.salesorderdetailid as int) as pk_salesorderdetail
+            , cast(source_salesorderdetail.productid as int) as fk_product
+            , cast(source_salesorderdetail.orderqty as int) as orderqty
+            , cast(source_salesorderdetail.unitprice as numeric(18,2)) as unitprice
+            , cast(source_salesorderdetail.unitpricediscount as numeric(18,2)) as unitpricediscount
+            --, cast(source_salesorderdetail.carriertrackingnumber as string) as carriertrackingnumber
+            --, cast(source_salesorderdetail.specialofferid as int) as fk_specialofferid
+            --, cast(source_salesorderdetail.rowguid as string) as rowguid
+            --, cast(source_salesorderdetail.modifieddate as date) as modifieddate
 
         from
             source_salesorderdetail
