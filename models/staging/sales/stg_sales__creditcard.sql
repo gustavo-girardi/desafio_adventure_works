51dbe6ed047318b472f45c94a808c8260485d088
Gustavo Girardi <gustavo.girardi@indicium.tech>
2025-03-29 20:28:04 +0000

    Exclusão das colunas sem utilidades

--- models/staging/sales/stg_sales__creditcard.sql
+++ models/staging/sales/stg_sales__creditcard.sql
@@ -9,10 +9,10 @@ renomeado as (
     select
         cast(source_creditcard.creditcardid as int) as pk_creditcard
         ,cast(source_creditcard.cardtype as string) as cardtype
-        ,cast(source_creditcard.cardnumber as int) as cardnumber
-        ,cast(source_creditcard.expmonth as int) as expmonth
-        ,cast(source_creditcard.expyear as int) as expyear
-        ,cast(source_creditcard.modifieddate as date) as modifieddate
+        --,cast(source_creditcard.cardnumber as int) as cardnumber
+        --,cast(source_creditcard.expmonth as int) as expmonth
+        --,cast(source_creditcard.expyear as int) as expyear
+        --,cast(source_creditcard.modifieddate as date) as modifieddate
     from source_creditcard
 )
 select * from renomeado
